9e6f0b655a992322f372b59b1c24958bc05565e3
Dagga78 <jeromejouet@laposte.net>
2019-01-11 11:01:00 +0100

    Login

--- app/src/main/java/com/example/jerom/controle_app/activity/SplashScreen.kt
+++ app/src/main/java/com/example/jerom/controle_app/activity/SplashScreen.kt
@@ -1,6 +1,8 @@
 package com.example.jerom.controle_app.activity
 
+import android.content.Intent
 import android.os.Bundle
+import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
 import com.example.jerom.controle_app.R
 
@@ -9,5 +11,13 @@ class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
+
+        Handler().postDelayed({
+            val intent = Intent(applicationContext, LoginActivity::class.java)
+            startActivity(intent)
+            //finish()
+
+        }, 1000)
     }
+
 }
